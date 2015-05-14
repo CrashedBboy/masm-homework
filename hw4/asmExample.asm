5325f249fad8cea965ac7dbadb20e22dbf3bb000
CrashedBboy <crashedbboy@gmail.com>
2015-05-14 20:08:38 +0800

    ver1

--- hw4/asmExample.asm
+++ hw4/asmExample.asm
@@ -1,11 +1,11 @@
-TITLE Example of ASM                (asmExample.ASM)
+TITLE Example of ASM	(asmExample.ASM)
 
 INCLUDE Irvine32.inc
 
 CountMatches PROTO,
-		arrayA:PTR BYTE,
-		arrayB:PTR BYTE,
-		arrayLength:DWORD
+	arrayA:PTR BYTE,
+	arrayB:PTR BYTE,
+	arrayLength:DWORD
 
 main          EQU start@0
 
@@ -14,13 +14,29 @@ main          EQU start@0
 	array2 BYTE 31, 34, 42, 4, 24, 1, 51, 3
 .code
 main PROC
-	;code detail
+	INVOKE CountMatches, ADDR array1, ADDR array2, LENGTHOF array1
+	call DumpRegs
 	exit
 main ENDP
 
-CountMatches PROC USES esi edi ecx,
-		arrayA:PTR BYTE,
-		arrayB:PTR BYTE,
-		arrayLength:DWORD
+CountMatches PROC USES edx esi edi ecx,
+					arrayA:PTR BYTE,
+					arrayB:PTR BYTE,
+					arrayLength:DWORD
+	mov eax, 0
+	mov esi, arrayA
+	mov edi, arrayB
+	mov ecx, arrayLength
+	L1:
+		mov dl, [esi]
+		cmp [edi], dl
+		jz EQUAL
+		jmp FORWARD
+	EQUAL:
+		inc eax
+	FORWARD:
+		inc esi
+		inc edi
+		loop L1
 CountMatches ENDP
 END main
